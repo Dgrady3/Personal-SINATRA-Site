fdf41d0ffbeb3ff6c51e61cbe75b796df1e8408d
Dgrady3 <joegrady1@gmail.com>
2014-12-10 21:06:25 -0500

    Initial commit

---
+++ me.rb
@@ -0,0 +1,21 @@
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader'
+
+###routes###
+get '/' do
+  erb :home
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/contact' do
+  erb :contact
+end
+
+
+get '/portfolio' do
+  erb :about
+end
